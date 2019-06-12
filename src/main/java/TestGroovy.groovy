58e43a74b3fe7e83f0ba9be08d6971e24b764892
petr.sloup <petr.sloup@inventi.cz>
2019-06-12 12:12:45 +0200

    initial commit - soapui, groovy project

---
+++ src/main/java/TestGroovy.groovy
@@ -0,0 +1,19 @@
+import com.eviware.soapui.model.testsuite.TestCaseRunner
+import com.eviware.soapui.model.testsuite.TestRunContext
+
+class Test {
+
+    public void testGroovy(TestRunContext testRunContext, TestCaseRunner testRunner) {
+
+        testSuiteRunner.testSuite.testCaseList[0].testStepList[0].getPropertyValue("")
+
+        log.info testRunner.testCase.testStepList[0].getPropertyValue("propertyTest")
+
+        testRunner.testCase.testSteps['PropertiesStep'].setPropertyValue("propertyTest", '255000')
+
+        log.info testRunner.testCase.testSteps['PropertiesStep'].getPropertyValue("propertyTest")
+
+    }
+
+
+}
